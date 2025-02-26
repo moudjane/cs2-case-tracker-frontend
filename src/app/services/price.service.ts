0c55d19edbc3f52bf41c9e1fe93c429b6d377911
moudjane <mathis.oudjane@gmail.com>
2025-02-26 14:50:58 +0100

    updated apiurl

--- src/app/services/price.service.ts
+++ src/app/services/price.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PriceService {
-  private apiUrl = 'http://localhost:5000/api';
+  private apiUrl = 'http://localhost:5001/api';
 
   constructor(private http: HttpClient) {}
 
